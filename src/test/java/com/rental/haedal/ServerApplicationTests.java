ad390988bde100f5a5182be65e650f86bc66b3c3
clcok <ekfrha24@gmail.com>
2025-02-24 10:34:27 +0900

    init

---
+++ src/test/java/com/rental/haedal/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.haedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
