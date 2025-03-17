d433c45688ed6c31deb6835c5b3abff43da17640
clcok <ekfrha24@gmail.com>
2025-03-17 23:15:25 +0900

    feat: DTO 작업을 위한 rental enum 추가

---
+++ src/main/java/com/rental/haedal/rental/domain/ItemCategory.java
@@ -0,0 +1,5 @@
+package com.rental.haedal.rental.domain;
+
+public enum ItemCategory {
+    BOOK, BOARD_GAME, OFFICE_SUPPLY, ETC
+}
---
+++ src/main/java/com/rental/haedal/rental/domain/ItemStatus.java
@@ -0,0 +1,5 @@
+package com.rental.haedal.rental.domain;
+
+public enum ItemStatus {
+    RENTAL_AVAILABLE, RENTING, OVERDUE
+}
