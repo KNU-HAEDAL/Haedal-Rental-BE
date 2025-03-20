10b2e2bac2bace4c24a0089b2a086600f1edda57
clcok <ekfrha24@gmail.com>
2025-03-20 22:29:06 +0900

    chore: Entity 수정에 따른 DTO 수정

--- src/main/java/com/rental/haedal/admin/dto/res/AdminItemListResponse.java
+++ src/main/java/com/rental/haedal/admin/dto/res/AdminItemListResponse.java
@@ -3,13 +3,13 @@ package com.rental.haedal.admin.dto.res;
 import com.rental.haedal.rental.domain.ItemCategory;
 import com.rental.haedal.rental.domain.ItemStatus;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public record AdminItemListResponse(
         ItemCategory itemCategory,
         String itemName,
         ItemStatus itemStatus,
-        Date rentalDate,
+        LocalDate returnDate,
         String rentalMemberName
 ) {
 }
--- src/main/java/com/rental/haedal/rental/dto/req/RentalRequest.java
+++ src/main/java/com/rental/haedal/rental/dto/req/RentalRequest.java
@@ -2,15 +2,14 @@ package com.rental.haedal.rental.dto.req;
 
 import com.rental.haedal.rental.domain.ItemCategory;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public record RentalRequest(
         String name,
         ItemCategory itemCategory,
-        Date rentalDate,
+        LocalDate rentalDate,
+        LocalDate returnDate,
         String picture
-
-        // 날짜 포매팅 수정해야함.
         // 대여 물품 사진을 찍어야함.
 ) {
 }
