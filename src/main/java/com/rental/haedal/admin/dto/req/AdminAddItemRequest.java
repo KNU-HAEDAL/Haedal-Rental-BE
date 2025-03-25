c9cc3e289fa320aa6a06dd84b5eaada1ac34fada
clcok <ekfrha24@gmail.com>
2025-03-25 22:54:42 +0900

    chore: Admin Request DTO 값 수정

--- src/main/java/com/rental/haedal/admin/dto/req/AdminAddItemRequest.java
+++ src/main/java/com/rental/haedal/admin/dto/req/AdminAddItemRequest.java
@@ -3,7 +3,7 @@ package com.rental.haedal.admin.dto.req;
 import com.rental.haedal.rental.domain.ItemCategory;
 
 public record AdminAddItemRequest(
-        String itemName,
+        Long itemId,
         ItemCategory itemCategory
 ) {
 }
--- src/main/java/com/rental/haedal/admin/dto/req/AdminItemRequest.java
+++ src/main/java/com/rental/haedal/admin/dto/req/AdminItemRequest.java
@@ -1,6 +1,6 @@
 package com.rental.haedal.admin.dto.req;
 
 public record AdminItemRequest(
-        String itemName
+        Long itemId
 ) {
 }
