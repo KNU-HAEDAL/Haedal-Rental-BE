9e757af990a55ac5ad410bdf1a15da41582fc394
yongminKwon1 <ym.kwon@lipis.kr>
2025-04-02 17:49:29 +0900

    fix: 반납 시 상태변경이 되지 않는 문제 수정

--- src/main/java/com/rental/haedal/repository/ItemRentalRepository.java
+++ src/main/java/com/rental/haedal/repository/ItemRentalRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRentalRepository extends JpaRepository<Rental, Long> {
-    List<Rental> findByItem_Id(Long itemId);
+    List<Rental> findAllByItem_Id(Long itemId);
 }
