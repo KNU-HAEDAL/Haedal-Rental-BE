6e81424667da652563aa5a69cab369e8a02a2032
clcok <ekfrha24@gmail.com>
2025-04-02 19:53:18 +0900

    style: 메서드 이름 동사형 변경

--- src/main/java/com/rental/haedal/service/ItemService.java
+++ src/main/java/com/rental/haedal/service/ItemService.java
@@ -67,7 +67,7 @@ public class ItemService {
         return response;
     }
 
-    public UserRentalResponse userRentalItem() {
+    public UserRentalResponse checkRentalItem() {
         Member member = authUtil.getCurrentUser();
 
         Integer penaltyCount = member.getPenaltyCount();
