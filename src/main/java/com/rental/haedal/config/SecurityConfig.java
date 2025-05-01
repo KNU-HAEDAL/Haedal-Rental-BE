7c26da061b8c4d9d6fa4b9265d2bf496c7e041b7
yongminKwon1 <ym.kwon@lipis.kr>
2025-05-01 14:45:18 +0900

    chore: cors 설정에 http://localhost:3000 추가

--- src/main/java/com/rental/haedal/config/SecurityConfig.java
+++ src/main/java/com/rental/haedal/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://haedal-rental.s3-website.ap-northeast-2.amazonaws.com"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://haedal-rental.s3-website.ap-northeast-2.amazonaws.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
